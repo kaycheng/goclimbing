950cc72bad9d307dd1cdfd6afd45c741446a0d9b
Kaycheng <kaycheng717@gmail.com>
2020-04-19 15:44:02 +0800

    Use ActiveStorage to storage picture and add image_processing gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # validation
   validates :username, presence: true
   validates_uniqueness_of :username
+
+  has_one_attached :avatar
+
 end
