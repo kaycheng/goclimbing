a7fb5348713069a1cb3ffa99f0b160d754c7ed7f
Kaycheng <kaycheng717@gmail.com>
2020-04-19 15:45:44 +0800

    Create user-page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,8 @@ class WelcomeController < ApplicationController
   def index
   end
   
+  def user
+    @user = User.find_by(username: params[:username])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 
     registrations: 'users/registrations' 
   }
+
+  # /@username
+  get '@:username', to: 'welcome#user', as: 'user_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
