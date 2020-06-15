0acb6633f7b24f00e75191643a0c3add868363e0
Kaycheng <kaycheng717@gmail.com>
2020-06-15 14:28:18 +0800

    Add participater on show page

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,9 @@
 module UserHelper
   def avatar(user, size)
-    image_tag user.avatar.variant(resize_to_limit: [size, size]), class: "avatar-#{size}" if user.avatar.attached?
+    if user.avatar.attached?
+      image_tag user.avatar.variant(resize_to_limit: [size, size]), class: "avatar-#{size}" 
+    else
+      user.username.split(%r{\s*}).first
+    end
   end
 end
